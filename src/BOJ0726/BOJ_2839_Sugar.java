66174f424d1d2a4634dc39e833358869fee98a6c
YunGyung Lee <yxxnkxx@gmail.com>
2022-07-26 17:41:49 +0900

    220726

--- src/BOJ0726/BOJ_2839_Sugar.java
+++ src/BOJ0726/BOJ_2839_Sugar.java
@@ -7,7 +7,7 @@ public class BOJ_2839_Sugar {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 
-		int max = N / 5; // 5kg의 최대값
+		int max = N / 5; // 5kg 사용 개수의 최대개수
 		int ans = -1;
 		for (int i = 0; i <= max; i++) {
 			if ((N - i * 5) % 3 == 0) { // 나머지가 딱 떨어짐
