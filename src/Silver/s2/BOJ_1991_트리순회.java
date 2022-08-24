edd507b97f22ef825e9a33f1f3cddb97835ee5ed
Lee YunGyung <yxxnkxx@gmail.com>
2022-08-24 13:51:03 +0900

    220824

--- src/Silver/s2/BOJ_1991_트리순회.java
+++ src/Silver/s2/BOJ_1991_트리순회.java
@@ -9,7 +9,7 @@ public class BOJ_1991_트리순회 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		String[][] tree = new String[26][2]; // data left right
+		String[][] tree = new String[26][3]; // data left right
 		tree[0][0] = "A";
 
 		for (int i = 0; i < N; i++) {
@@ -17,15 +17,9 @@ public class BOJ_1991_트리순회 {
 			String p = st.nextToken();
 			String left = st.nextToken();
 			String right = st.nextToken();
-			int pIdx = 0;
-			for (int j = 0; j < 26; j++) {
-				if (tree[j][0] != null && tree[j][0].equals(p)) {
-					pIdx = j;
-					break;
-				}
-			}
+			int pIdx = p.charAt(0) - 'A';
 
-			// 부모 인덱스 찾기
+			// 각 알파벳의 index는 A가 0, B가 1 ...
 			if (!left.equals(".")) {
 				tree[left.charAt(0) - 'A'][0] = left;
 				tree[pIdx][1] = left;
@@ -35,11 +29,12 @@ public class BOJ_1991_트리순회 {
 				tree[pIdx][2] = right;
 			}
 		} // tree 입력
-		preOrder(tree, 1);
+
+		preOrder(tree, 0);
 		sb.append("\n");
-		inOrder(tree, 1);
+		inOrder(tree, 0);
 		sb.append("\n");
-		postOrder(tree, 1);
+		postOrder(tree, 0);
 		System.out.println(sb.toString());
 	}
 
@@ -47,25 +42,31 @@ public class BOJ_1991_트리순회 {
 
 	static void preOrder(String[][] tree, int i) {
 		if (tree[i][0] != null) {
-			sb.append(tree[i]);
-			preOrder(tree, i * 2);
-			preOrder(tree, i * 2 + 1);
+			sb.append(tree[i][0]);
+			if (tree[i][1] != null)
+				preOrder(tree, tree[i][1].charAt(0) - 'A');
+			if (tree[i][2] != null)
+				preOrder(tree, tree[i][2].charAt(0) - 'A');
 		}
 	}
 
-	static void inOrder(String[] tree, int i) {
-		if (i <= tree.length && tree[i] != null) {
-			inOrder(tree, i * 2);
-			sb.append(tree[i]);
-			inOrder(tree, i * 2 + 1);
+	static void inOrder(String[][] tree, int i) {
+		if (tree[i][0] != null) {
+			if (tree[i][1] != null)
+				inOrder(tree, tree[i][1].charAt(0) - 'A');
+			sb.append(tree[i][0]);
+			if (tree[i][2] != null)
+				inOrder(tree, tree[i][2].charAt(0) - 'A');
 		}
 	}
 
-	static void postOrder(String[] tree, int i) {
-		if (i <= tree.length && tree[i] != null) {
-			postOrder(tree, i * 2);
-			postOrder(tree, i * 2 + 1);
-			sb.append(tree[i]);
+	static void postOrder(String[][] tree, int i) {
+		if (tree[i][0] != null) {
+			if (tree[i][1] != null)
+				postOrder(tree, tree[i][1].charAt(0) - 'A');
+			if (tree[i][2] != null)
+				postOrder(tree, tree[i][2].charAt(0) - 'A');
+			sb.append(tree[i][0]);
 		}
 	}
 
