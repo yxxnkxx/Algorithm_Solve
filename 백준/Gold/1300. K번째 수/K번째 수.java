9c19cf0bf710befca9fd4c58755293a8a7469772
YunGyung Lee <109255398+yxxnkxx@users.noreply.github.com>
2022-09-07 01:35:43 +0900

    [Gold II] Title: K번째 수, Time: 148 ms, Memory: 12896 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		long N = sc.nextInt();
-		long K = sc.nextInt();
-
+		long N = sc.nextLong();
+		long K = sc.nextLong();
 		long left = 1;
 		long right = K;
 		long ans = 0;
