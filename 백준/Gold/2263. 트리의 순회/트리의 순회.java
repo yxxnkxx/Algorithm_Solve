e6424455ef01a5c78e599f185fa7121a5a107789
YunGyung Lee <109255398+yxxnkxx@users.noreply.github.com>
2022-09-12 17:56:35 +0900

    [Gold II] Title: 트리의 순회, Time: 428 ms, Memory: 69800 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -7,14 +7,12 @@ public class Main {
 	static int[] in;
 	static int[] post;
 	static int N;
-	static boolean[] visited;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		in = new int[N + 1];
 		post = new int[N];
-		visited = new boolean[N + 1];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++)
 			in[Integer.parseInt(st.nextToken())] = i;
@@ -28,17 +26,16 @@ public class Main {
 	static StringBuilder sb = new StringBuilder();
 
 	static void preOrder(int left, int right, int cnt) {
-		if (left <= right) {
-			int root = post[right];
-			if (visited[root])
-				return;
+		if (left > right)
+			return;
 
-			int mid = 0; // in에서 root의 index
-			mid = in[root];
-			visited[root] = true;
-			sb.append(root + " ");
-			preOrder(left, mid - 1 - cnt, cnt);
-			preOrder(mid - cnt, right - 1, cnt + 1);
-		}
+		int root = post[right];
+
+		int mid = 0; // in에서 root의 index
+		mid = in[root];
+		sb.append(root + " ");
+		preOrder(left, mid - 1 - cnt, cnt);
+		preOrder(mid - cnt, right - 1, cnt + 1);
+
 	}
 }
