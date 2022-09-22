ef90d9c18d77d3fbfc10dfb35b461c0a8e6df95b
Lee YunGyung <yxxnkxx@gmail.com>
2022-09-22 09:05:34 +0900

    merge

--- src/Silver/s1/BOJ_1149_RGB거리.java
+++ src/Silver/s1/BOJ_1149_RGB거리.java
@@ -3,27 +3,16 @@ package Silver.s1;
 import java.util.Scanner;
 
 public class BOJ_1149_RGB거리 {
-<<<<<<< HEAD
-	static int N;
-	static int[][] color;
-	static int[] dp;
-=======
-
 	static int N;
 	static int[][] color;
 	static int[][] dp;
->>>>>>> a4d5eb4813135e0a0276ff48eaf0e8b790a089ad
 	static int ans;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		color = new int[N][3];
-<<<<<<< HEAD
-		dp = new int[N];
-=======
 		dp = new int[N][3];
->>>>>>> a4d5eb4813135e0a0276ff48eaf0e8b790a089ad
 		ans = Integer.MAX_VALUE;
 
 		for (int i = 0; i < N; i++)
@@ -31,30 +20,6 @@ public class BOJ_1149_RGB거리 {
 				color[i][j] = sc.nextInt();
 
 		for (int i = 0; i < 3; i++) {
-<<<<<<< HEAD
-			dp[0] = color[0][i];
-			find(0, i);
-		}
-		System.out.println(ans);
-	}
-
-	static void find(int idx, int c) {
-		if (idx == N) {
-			if (dp[idx - 1] < ans)
-				ans = dp[idx - 1];
-			return;
-		}
-		// 최소값 확인
-		if (idx != 0)
-			dp[idx] = dp[idx - 1] + color[idx][c];
-
-		if (dp[idx] < ans)
-			for (int i = 0; i < 3; i++)
-				if (i != c)
-					find(idx + 1, i);
-	}
-}
-=======
 			dp[0][i] = color[0][i];
 		}
 
@@ -82,4 +47,3 @@ public class BOJ_1149_RGB거리 {
 		return dp[idx][c];
 	}
 }
->>>>>>> a4d5eb4813135e0a0276ff48eaf0e8b790a089ad
