98939f32da189563ca1e1af7090eee50116ed393
YunGyung Lee <109255398+yxxnkxx@users.noreply.github.com>
2022-09-27 15:47:05 +0900

    [Gold III] Title: 나머지 합, Time: 440 ms, Memory: 160148 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -13,16 +13,13 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 
-		long[] res = new long[N];
-
 		long[] cnt = new long[M];
 		cnt[0] = 1;
 		st = new StringTokenizer(br.readLine());
-		res[0] = Long.parseLong(st.nextToken()) % M;
-		cnt[(int) res[0]]++;
-		for (int i = 1; i < N; i++) {
-			res[i] = (res[i - 1] + Long.parseLong(st.nextToken())) % M;
-			cnt[(int) res[i]]++;
+		long curr = 0;
+		for (int i = 0; i < N; i++) {
+			curr = (curr + Long.parseLong(st.nextToken())) % M;
+			cnt[(int) curr]++;
 		}
 
 		long ans = 0;
