0098686e8c6fe8ca919fd5d93155350381b6d553
YunGyung Lee <109255398+yxxnkxx@users.noreply.github.com>
2022-09-27 17:12:30 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 728 ms, Memory: 127496 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,26 +1,34 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
 		int[][] map = new int[N + 1][N + 1];
 
-		for (int i = 1; i <= N; i++)
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= N; j++) {
-				map[i][j] = map[i - 1][j] + map[i][j - 1] + sc.nextInt() - map[i - 1][j - 1];
+				map[i][j] = map[i - 1][j] + map[i][j - 1] - map[i - 1][j - 1] + Integer.parseInt(st.nextToken());
 			}
-
-		while (M-- > 0) {
-			int x1 = sc.nextInt();
-			int y1 = sc.nextInt();
-			int x2 = sc.nextInt();
-			int y2 = sc.nextInt();
-			System.out.println(map[x2][y2] - map[x1 - 1][y2] - map[x2][y1 - 1] + map[x1 - 1][y1 - 1]);
 		}
+		StringBuilder sb = new StringBuilder();
+		while (M-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			sb.append(map[x2][y2] - map[x1 - 1][y2] - map[x2][y1 - 1] + map[x1 - 1][y1 - 1]).append("\n");
+		}
+		System.out.print(sb);
 
 	}
 
