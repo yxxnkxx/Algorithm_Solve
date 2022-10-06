a1d0898bec726ae2649d667b64a18b4491486e60
YunGyung Lee <109255398+yxxnkxx@users.noreply.github.com>
2022-10-06 09:15:00 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 108 ms, Memory: 12852 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] dp = new int[11]; // 입력 범위 1~10
+		dp[0] = 1;
+		// 1로 구하기
+		for (int n = 1; n <= 10; n++) {
+			for (int i = 1; i <= 3; i++)
+				if (n - i >= 0)
+					dp[n] += dp[n - i];
+		}
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++)
+			System.out.println(dp[sc.nextInt()]);
+
+	}
+
+}
