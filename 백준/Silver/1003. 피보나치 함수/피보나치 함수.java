9ad7f5e4de3584154d98caa7d5ace4d55e462ee0
YunGyung Lee <109255398+yxxnkxx@users.noreply.github.com>
2022-10-06 10:10:20 +0900

    [Silver III] Title: 피보나치 함수, Time: 116 ms, Memory: 13004 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int[][] dp = new int[41][2];
+		dp[0][0] = 1;
+		dp[1][1] = 1;
+		for (int i = 2; i <= 40; i++) {
+			dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+			dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+		}
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		for (int t = 0; t < T; t++) {
+			int i = sc.nextInt();
+			sb.append(dp[i][0]);
+			sb.append(" ");
+			sb.append(dp[i][1]).append("\n");
+
+		}
+
+		System.out.print(sb);
+	}
+
+}
