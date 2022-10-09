16ef1de23e2a86a15b03d4f9cee30be809e6ed78
YunGyung Lee <109255398+yxxnkxx@users.noreply.github.com>
2022-10-10 00:17:22 +0900

    [Silver V] Title: 집합, Time: 3476 ms, Memory: 348676 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int M = sc.nextInt();
+		int num = 0;
+		StringBuilder sb = new StringBuilder();
+		while (M-- > 0) {
+			String comm = sc.next();
+			int x = 0;
+			if (!comm.equals("all") && !comm.equals("empty"))
+				x = sc.nextInt();
+			switch (comm) {
+			case "add":
+				num = num | (1 << x);
+				break;
+			case "remove":
+				num = num & (~(1 << x));
+				break;
+			case "check":
+				if ((num & (1 << x)) == 0)
+					sb.append(0).append("\n");
+				else
+					sb.append(1).append("\n");
+				break;
+			case "toggle":
+				num = (num ^ (1 << x));
+				break;
+			case "all":
+				num = (1 << 21) - 1;
+				break;
+			case "empty":
+				num = 0;
+				break;
+			}
+		}
+		System.out.print(sb);
+	}
+
+}
