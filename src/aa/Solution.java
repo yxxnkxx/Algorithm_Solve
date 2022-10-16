fd9a6caf93b00dc29bbe76b05c8f39dd0cb91d4a
YunGyung Lee <yxxnkxx@gmail.com>
2022-10-17 00:30:12 +0900

    files

--- src/aa/Solution.java
+++ src/aa/Solution.java
@@ -1,10 +1,9 @@
+package aa;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Solution {
 
@@ -17,6 +16,7 @@ public class Solution {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
+
 
 		for (int t = 1; t <= T; t++) {
 			N = Integer.parseInt(br.readLine());
