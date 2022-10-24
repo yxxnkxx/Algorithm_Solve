c640977c196cf07126d7b0b5009fb5703ee72581
YunGyung Lee <109255398+yxxnkxx@users.noreply.github.com>
2022-10-24 14:26:42 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.30 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int solution(String dartResult) {
+        String[] input = dartResult.split("[^0-9]+");
+        int[] nums = new int[input.length];
+        for (int i = 0; i < input.length; i++)
+            nums[i] = Integer.parseInt(input[i]);
+
+        String[] comm = dartResult.split("[0-9]+");
+        
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            char bonus = comm[i + 1].charAt(0);
+            int mul = 0;
+            switch (bonus) {
+                case 'S':
+                    mul = 1;
+                    break;
+                case 'D':
+                    mul = 2;
+                    break;
+                case 'T':
+                    mul = 3;
+                    break;
+            }
+
+            nums[i] = (int) (Math.pow(num, mul));
+            if (comm[i + 1].length() == 2) {
+                // option o
+                char opt = comm[i + 1].charAt(1);
+                if (opt == '*') {
+                    nums[i] *= 2;
+                    if (i > 0)
+                        nums[i - 1] *= 2;
+                   
+                } else {
+                    nums[i] *= -1;
+                }
+
+            }
+
+        }
+
+        int answer = 0;
+        for (Integer num : nums)
+            answer += num;
+
+
+        return answer;
+    }
+}
