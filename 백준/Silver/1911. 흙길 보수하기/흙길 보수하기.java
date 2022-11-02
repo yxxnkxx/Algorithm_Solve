ecca1ea44e9d6fcd88177ce2a81a72db1e1aec67
YunGyung Lee <109255398+yxxnkxx@users.noreply.github.com>
2022-11-02 22:09:07 +0900

    [Silver I] Title: 흙길 보수하기, Time: 436 ms, Memory: 35820 KB -BaekjoonHub

---
+++ 백준/Silver/1911. 흙길 보수하기/흙길 보수하기.java
@@ -0,0 +1,38 @@
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int L = sc.nextInt();
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0])
+                    return o1[1] - o2[1];
+                return o1[0] - o2[0];
+            }
+        });
+        int cnt = 0;
+        int curr = 0; // 널판지의 끝 좌표
+        for (int i = 0; i < N; i++) {
+
+            int st = Math.max(arr[i][0], curr);
+            int ed = arr[i][1];
+            if (ed <= curr) continue;
+            int tmp = ((ed - st) % L != 0) ? ((ed - st) / L) + 1 : (ed - st) / L;
+            curr = st + tmp * L;
+            cnt += tmp;
+        }
+        System.out.println(cnt);
+    }
+}
