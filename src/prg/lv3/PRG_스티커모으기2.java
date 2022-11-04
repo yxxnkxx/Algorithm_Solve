a2e237b88de492dda489634c655797f16d09683e
YunGyung Lee <yxxnkxx@gmail.com>
2022-11-04 17:49:02 +0900

    221104 file

---
+++ src/prg/lv3/PRG_스티커모으기2.java
@@ -0,0 +1,17 @@
+package prg.lv3;
+
+import java.util.Arrays;
+
+public class PRG_스티커모으기2 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 3, 2, 5, 4}));
+    }
+
+    public static int solution(int[] stickers) {
+
+
+    }
+
+}
+
