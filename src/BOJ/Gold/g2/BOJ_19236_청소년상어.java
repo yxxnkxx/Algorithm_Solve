10b43803568c48f09e6cec2f09434ad48e5ba9d9
YunGyung Lee <yxxnkxx@gmail.com>
2022-11-08 23:16:08 +0900

    221108 file

---
+++ src/BOJ/Gold/g2/BOJ_19236_청소년상어.java
@@ -0,0 +1,13 @@
+package BOJ.Gold.g2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_19236_청소년상어 {
+    public static void main(String[] args) {
+
+    }
+
+}
