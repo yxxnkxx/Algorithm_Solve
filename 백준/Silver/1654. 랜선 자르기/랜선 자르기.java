97749dae2a101af578cad3fb1299419591e2585f
YunGyung Lee <109255398+yxxnkxx@users.noreply.github.com>
2022-12-02 22:18:02 +0900

    [Silver II] Title: 랜선 자르기, Time: 320 ms, Memory: 27568 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        int N = sc.nextInt();
+        long[] arr = new long[K];
+        for (int i = 0; i < K; i++)
+            arr[i] = sc.nextLong();
+        long start = 1;
+        long end = (long) Math.pow(2, 31) + 1;
+        long ans = 0;
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long tmp = 0;
+            for (int i = 0; i < K; i++)
+                tmp += arr[i] / mid;
+            if (tmp >= N) {
+                ans = mid;
+                start = mid + 1;
+            } else end = mid - 1;
+        }
+        System.out.println(ans);
+    }
+}
