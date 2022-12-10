843f0c75f2cf8c4539307fd3df8562d2df15c45a
YunGyung Lee <yxxnkxx@gmail.com>
2022-12-10 21:13:54 +0900

    221210 file

--- src/BOJ/Gold/g5/BOJ_3079_입국심사.java
+++ src/BOJ/Gold/g5/BOJ_3079_입국심사.java
@@ -1,7 +1,43 @@
 package BOJ.Gold.g5;
 
+import java.util.Scanner;
+
 public class BOJ_3079_입국심사 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long M = sc.nextLong();
+        long[] time = new long[N];
+        for (int i = 0; i < N; i++)
+            time[i] = sc.nextLong();
+        long start = 1;
+        long end = 1_000_000_000L * M;
+        long ans = end;
+        out:
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            // mid안에 가능한지 확인
+            long tmp = 0;
+            for (int i = 0; i < N; i++) {
+                tmp += mid / time[i];
+                if (tmp >= M) {
+                    // mid분 안에 가능
+                    ans = mid;
+                    end = mid - 1;
+                    continue out;
+                }
+            }
+            if (tmp >= M) {
+                // mid분 안에 가능
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        System.out.println(ans);
 
     }
 }
