7a4ce12c1666d04c8c0cc4929f08cdc7725e65d2
YunGyung Lee <yxxnkxx@gmail.com>
2022-12-14 17:54:31 +0900

    221214 file

---
+++ src/BOJ/Gold/g3/BOJ_7570_줄세우기.java
@@ -0,0 +1,43 @@
+package BOJ.Gold.g3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_7570_줄세우기 {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+
+        int ans = 1;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        boolean[] visited = new boolean[N + 1];
+        for (int i = 0; i < N; i++) {
+            int val = arr[i];
+            visited[val] = true;
+            if (map.containsKey(val - 1)) {
+                map.put(val, map.get(val - 1) + 1);
+                ans = Math.max(map.get(val - 1) + 1, ans);
+                map.remove(val - 1);
+            } else {
+                if (val == N || visited[val + 1]) {
+                    continue;
+                }
+                map.put(val, 1);
+            }
+
+        }
+        System.out.println(N - ans);
+
+
+    }
+
+}
