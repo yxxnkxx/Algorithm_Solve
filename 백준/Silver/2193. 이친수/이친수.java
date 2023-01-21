c4147d8eb6181f5fce391341ce66c964bde23be1
YunGyung Lee <109255398+yxxnkxx@users.noreply.github.com>
2023-01-21 16:55:31 +0900

    [Silver III] Title: 이친수, Time: 120 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long[][] arr = new long[N + 1][2]; // 0으로 끝나는 수, 1로 끝나는 수
+        arr[1][1] = 1;
+        for (int i = 2; i <= N; i++) {
+            arr[i][0] = arr[i - 1][0] + arr[i - 1][1];
+            arr[i][1] = arr[i - 1][0];
+        }
+        System.out.println(arr[N][0] + arr[N][1]);
+
+    }
+}
